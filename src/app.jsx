1c4e56f14be7293257e87e6dab80ca07f441d79e
jackzampolin <jack.zampolin@gmail.com>
2015-08-21 08:16:40 -0700

    minor changes

--- src/app.jsx
+++ src/app.jsx
@@ -3,10 +3,6 @@ var React = require('react');
 var Routes = require('./routes');
 var farmName = require('./utils/staticAssets/farmInfo').name
 
-String.prototype.toProperCase = function () {
-  return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-};
-
 document.getElementsByTagName('title')[0].text = farmName
 
 React.render(Routes, document.querySelector('#app'));
